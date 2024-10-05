38930c1e9d2086cc4b8decafeabca70c5ce3c905
Aqsa Amjad <aqsa_amjad@outlook.com>
2024-10-05 16:43:13 +0100

    tested the addTask() method

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        // Create an instance of TaskManager
+        TaskManager taskManager = new TaskManager();
+
+        // Call the addTask method to add a new task
+        taskManager.addTask("Buy groceries");
+
+        // Add more tasks if needed
+        taskManager.addTask("Write Java code");
+        taskManager.addTask("Read a book");
+    }
+}
