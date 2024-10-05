2975d753e07559c1806f0affc206327393f3ccd6
Aqsa Amjad <aqsa_amjad@outlook.com>
2024-10-05 16:01:04 +0100

    implemented saveTasks() method of TaskManager class
    saves tasks to the JSON file

--- src/TaskManager.java
+++ src/TaskManager.java
@@ -10,7 +10,7 @@ public class TaskManager {
     // constructor - loads tasks from the file when TaskManager is created
     public TaskManager() {
         tasks = loadTasks();
-    }
+    }   
 
     // loads tasks from the JSON file
     private List<Task> loadTasks() {
@@ -39,5 +39,23 @@ public class TaskManager {
             System.out.println("No tasks file found, starting fresh.");
         }
         return taskList; // return the list of tasks (empty if no tasks were loaded)
+    }
+
+    // saves tasks to the JSON file
+    private void saveTasks() {
+        StringBuilder json = new StringBuilder("[");
+        for (int i = 0; i < tasks.size(); i++) {
+            json.append(tasks.get(i).toJson());
+            if (i < tasks.size() - 1) {
+                json.append(",");
+            }
+        }
+        json.append("]");
+
+        try {
+            Files.write(Paths.get(TASK_FILE_PATH), json.toString().getBytes());
+        } catch (IOException e) {
+            System.out.println("Error writing to tasks file.");
+        }
     }
 }
