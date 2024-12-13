b1a39ae93e457ca1d3c5090facda58dff52052e8
Aqsa Amjad <aqsa_amjad@outlook.com>
2024-12-13 00:42:40 +0000

    implement Task & TaskStatus

---
+++ src/tasktracker/Task.java
@@ -0,0 +1,62 @@
+package tasktracker;
+
+import java.time.LocalDateTime;
+
+public class Task {
+    private int id;
+    private String description;
+    private TaskStatus status; // use enum to prevent invalid status assignments
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+
+    // constructor
+    public Task(int id, String description) {
+        this.id = id;
+        this.description = description;
+        this.status = TaskStatus.TODO;
+        this.createdAt = LocalDateTime.now();
+        this.updatedAt = LocalDateTime.now();
+    }
+
+    // getters and setters
+    public int getId() {
+        return id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+        this.updatedAt = LocalDateTime.now();
+    }
+
+    public TaskStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(TaskStatus status) {
+        this.status = status;
+        this.updatedAt = LocalDateTime.now();
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public LocalDateTime getUpdatedAt() {
+        return updatedAt;
+    }
+
+    @Override
+    public String toString() {
+        return "Task{" +
+                "id=" + id +
+                ", description='" + description + '\'' +
+                ", status=" + status +
+                ", createdAt=" + createdAt +
+                ", updatedAt=" + updatedAt +
+                '}';
+    }
+}
